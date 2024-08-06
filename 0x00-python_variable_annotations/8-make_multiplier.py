68375ab29c8e971001433f2ef89dcdbb5efa0320
linetgitonga <linetgitonga55@gmail.com>
2024-08-06 01:45:55 +0000

    annotations

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Task 8's module.
+'''
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Creates a multiplier function.
+    '''
+    return lambda x: x * multiplier
